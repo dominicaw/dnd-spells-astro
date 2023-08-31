403326d619e1c75dec27fedb70cba4cd09732f58
Dominica Wannenburg <dominica@offerzen.com>
2023-09-01 00:22:47 +0200

    Added ClassEmoji helper , and ClassesGrid component

---
+++ src/components/ClassesGrid.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Class } from '../interfaces/class';
+  import { classEmoji } from '../helpers/classEmoji';
+  export let classes: Class[];
+  
+  type ClassIndex = keyof typeof classEmoji;
+</script>
+
+<section>
+  <div class="grid gap-6 grid-cols-3">
+    {#each classes as currentClass}
+      <div class="bg-white shadow-lg shadow-stone-200 p-3 rounded">
+        {#if currentClass.index in classEmoji}
+          <div class="text-[40px]">{classEmoji[currentClass.index]}</div>
+        {/if}
+        <p>Name: {currentClass.name}</p>
+        <p>Hit Dice: d{currentClass.hit_die}</p>
+      </div>
+    {/each}
+  </div>
+</section>
+
+<style>
+</style>
