5ebda31bfe89b44cc4f3b4816c07a78a2a9cbe1e
Dominica Wannenburg <dominica@offerzen.com>
2023-09-01 10:31:45 +0200

    Added links for the class pages

--- src/components/ClassesGrid.svelte
+++ src/components/ClassesGrid.svelte
@@ -7,12 +7,14 @@
 <section>
   <div class="grid gap-6 grid-cols-1 md:grid-cols-3">
     {#each classes as currentClass}
+    <a href={currentClass?.index}>
       <div class="bg-white shadow-lg shadow-stone-200 p-3 rounded">
         {#if currentClass.index in classEmoji}
           <div class="text-[40px]">{classEmoji[currentClass.index]}</div>
         {/if}
         <p>{currentClass.name}</p>
       </div>
+      </a>
     {/each}
   </div>
 </section>
