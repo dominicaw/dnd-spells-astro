c1a19d162c9672ba61defce7723cd4469b8bd0ae
Dominica Wannenburg <dominicawan@gmail.com>
2023-09-05 18:56:53 +0200

    New page setup for individual spells

--- src/components/SpellsGrid/SpellsGrid.svelte
+++ src/components/SpellsGrid/SpellsGrid.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section>
-  <div class="grid gap-8 grid-cols-1 md:grid-cols-4">
+  <div class="grid gap-8 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
     {#each spells as currentSpell}
       <SpellCard {currentSpell} />
     {/each}
